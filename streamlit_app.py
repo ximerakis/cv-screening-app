993a162031052a8bab3f025a2a04d8d5e1e00af4
ximerakis <ximerakis.kyriakos@gmail.com>
2025-05-13 14:01:25 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,10 @@ st.title("📄 CV Screening Assistant")
 
 api_key = st.text_input("🔐 Enter your OpenAI API Key", type="password")
 client = OpenAI(api_key=api_key)
+st.subheader("📤 Email Configuration")
+gmail_address = st.text_input("Your Gmail address (sender)", type="default")
+gmail_password = st.text_input("Gmail App Password", type="password")
+
 
 st.subheader("📤 Email Configuration")
 gmail_address = st.text_input("Your Gmail address (sender)", type="default")
